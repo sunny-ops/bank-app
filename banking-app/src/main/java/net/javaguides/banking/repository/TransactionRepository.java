2a7c59b841ed21111f7032504dc1dd4171c9c125
shizhong <shizhong1989@gmail.com>
2024-05-16 14:11:21 -0700

    Create TransactionRepository

---
+++ banking-app/src/main/java/net/javaguides/banking/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package net.javaguides.banking.repository;
+
+import net.javaguides.banking.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountIdOrderByTimestampDesc(Long accountId);
+
+}
+// Long is the primary key -- id
